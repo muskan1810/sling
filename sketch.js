61289651087b0d2aa496f87364d46d9cc16e704e
muskan1810 <muskan.thakur.1995@gmail.com>
2019-11-25 19:33:14 +0530

    sketch

--- sketch.js
+++ sketch.js
@@ -36,10 +36,10 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200,50);
 
-    log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
+    //log6 = new Log(230,180,80, PI/2);
+    slingShot = new chain(bird.body,{x:200, y:50});
 }
 
 function draw(){
@@ -63,6 +63,13 @@ function draw(){
 
     bird.display();
     platform.display();
-    log6.display();
-    chain.display();    
+   // log6.display();
+    slingShot.display();    
+}
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body, {x:mouseX, y:mouseY});
+}
+
+function mouseReleased(){
+    slingShot.fly();
 }
